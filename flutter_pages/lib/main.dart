ef04db319185cd3c96f7bfc75ef0ddb79672885a
Bhavesh Awal <bhaveshawalba9@gmail.com>
2023-01-22 18:13:38 +0530

    updated

--- flutter_pages/lib/main.dart
+++ flutter_pages/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pages/screens/login/login.dart';
+import 'package:flutter_pages/utils/our_theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OurLogin(),      
+      theme: OurTheme().buildTheme(),
+      home: OurLogin(),
     );
   }
 }
---
+++ flutter_pages/lib/utils/our_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OurTheme {
+  Color hehe = const Color.fromRGBO(0, 254, 178, 1);
+  ThemeData buildTheme() {
+    return ThemeData(
+      canvasColor: hehe,
+    );
+  }
+}
